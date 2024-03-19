0f4dbf12de12482ce6a8da0ce92d8b6d8f65d785
Himanshu <him2282001@gmail.com>
2024-03-19 14:43:03 +0530

    Add generic insert and delete queries

--- src/main/java/com/himanshu/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/himanshu/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,6 +1,7 @@
 package com.himanshu.springboot.learnjpaandhibernate.course.jdbc;
 
 import com.himanshu.springboot.learnjpaandhibernate.course.jdbc.repository.CourseJdbcRepository;
+import com.himanshu.springboot.learnjpaandhibernate.course.models.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,11 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1, "Learn AWS", "in28minutes"));
+        repository.insert(new Course(2, "Learn Azure", "in28minutes"));
+        repository.insert(new Course(3, "Learn DevOps", "in28minutes"));
+
+        repository.deleteById(1);
+
     }
 }
